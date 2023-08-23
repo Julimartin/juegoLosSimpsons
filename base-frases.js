17edd59f84ee5b1f48d6985ef0889e174195bf7e
Julimartin <martin.julieta@hotmail.com>
2023-08-23 18:59:17 -0300

    primer upload

---
+++ base-frases.js
@@ -0,0 +1,51 @@
+let baseDeFrases = [
+    {
+    nivel: "Principiante",
+    frase: '"Ahh, soy un pastelito horneado de canela"',
+    respuesta: "https://i.ibb.co/CsVxxF6/homero.jpg",
+    distractores: ["https://i.ibb.co/5B59htf/bart.jpg",
+                    "https://i.ibb.co/HT25WY6/maude-Flanders.jpg" ],
+    
+    },
+    {
+        nivel: "Intermedio",
+        frase: '"Yo no lo hices!!"',
+        respuesta: "https://i.ibb.co/W5jrZxH/apu.jpg",
+        distractores: ["https://i.ibb.co/P6BQbnD/mclure.jpg",
+                        "https://i.ibb.co/cQ2qX0f/smiders.jpg" ],
+        
+    },
+    {
+        nivel: "Avanzado",
+        frase: '"No me interesa comprar su casa. Pero quisiera utilizar su baño, ojear sus revistas, reordenar sus figuras y manosear su comida de forma antihigiénica. ¡Ja! Ahora saben lo que se siente."',
+        respuesta: "https://i.ibb.co/W5jrZxH/apu.jpg",
+        distractores: ["https://i.ibb.co/HVx5BFL/bob-pati-o.jpg",
+                        "https://i.ibb.co/z6LrGnL/willy.jpg" ],
+        
+    },
+    {
+        nivel: "Avanzado",
+        frase: '"Hundiste mi acorazado"',
+        respuesta: "https://i.ibb.co/j8Vkm2w/Jasper-Beardsley.jpg",
+        distractores: ["https://i.ibb.co/rkvz3JR/rapha.jpg",
+                        "https://i.ibb.co/0DwN6RD/reverendo.jpg" ],
+        
+    },
+    {
+        nivel: "Avanzado",
+        frase: '"Mi vieja mula ya no es lo que era, ya no es lo que era..."',
+        respuesta: "https://i.ibb.co/jkr4T31/El-viejo-jud-o.jpg",
+        distractores: ["https://i.ibb.co/rHv8SFB/seymour-Skinner.jpg",
+                        "https://i.ibb.co/nrGTzbx/edna.jpg"],
+        
+    },
+    {
+        nivel: "Principiante",
+        frase: '“¡Que elegancia la de Francia!”',
+        respuesta: "https://i.ibb.co/CsVxxF6/homero.jpg",
+        distractores: ["https://i.ibb.co/C2XT7bp/kent-Brokman.jpg",
+                        "https://i.ibb.co/y0k4SZ4/discoStu.jpg"],
+        
+    },
+
+]
---
+++ index.js
@@ -0,0 +1,62 @@
+let INDEX_FRASE = 0;    
+let puntaje = 0;
+elegirFrase(INDEX_FRASE);
+
+function elegirFrase(index) {
+    objetoFrase = baseDeFrases[index];
+    opciones = [...objetoFrase.distractores];
+    opciones.push(objetoFrase.respuesta)
+
+    opciones.sort(()=> Math.random()-0.5); //desorganiza las opciones
+    document.getElementById("nivel").innerHTML = objetoFrase.nivel;
+    document.getElementById("frase").innerHTML = objetoFrase.frase;
+    document.getElementById("btn1").innerHTML = `<img src="${opciones[0]}">`;
+    document.getElementById("btn2").innerHTML = `<img src="${opciones[1]}">`;
+    document.getElementById("btn3").innerHTML = `<img src="${opciones[2]}">`;
+}
+
+async function seleccionarOpcion(index) {
+    if (respuestaValida(index)) {
+        await Swal.fire({
+            icon: 'success',
+            title: 'Super!',
+            text: 'Acertaste',
+        })
+        puntaje++;
+    } else {
+        await Swal.fire({
+            icon: 'error',
+            title: 'Oops...',
+            text: 'casi lo logras',
+        })
+    }
+    INDEX_FRASE++;
+
+    if (finalDelJuego()) {
+        await Swal.fire({
+            title: 'Juego terminado',
+            text: `tu puntaje fue de ${puntaje}/${baseDeFrases.length}`,
+        })
+        
+    }
+    reiniciarJuego(); 
+    }
+
+
+function respuestaValida(index) {
+    return opciones[index] == objetoFrase.respuesta;
+}
+
+function finalDelJuego() {
+    return INDEX_FRASE == baseDeFrases.length;
+}
+
+var botonReinicio = document.getElementById("reiniciar");
+
+function reiniciarJuego() {
+    INDEX_FRASE = 0;
+    puntaje = 0;
+    elegirFrase(INDEX_FRASE);
+}
+
+botonReinicio.addEventListener("click", reiniciarJuego);
